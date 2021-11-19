49e5dac50dc9b6c5b8b636f6cb305aaa98e82de1
Antoine Queru <antoine.queru@zetruc.fr>
2021-11-19 15:27:14 +0100

    Feature - add netlify identity in head

--- pages/index.vue
+++ pages/index.vue
@@ -48,6 +48,16 @@
 </main>
 </template>
 
+<script>
+export default {
+    head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+}
+</script>
+
 <style>
 * {
     box-sizing: border-box;
