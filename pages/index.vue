7552f9eaa101d888874914780a6ebe5037fe4268
Antoine Queru <antoine.queru@gmail.com>
2021-11-19 18:41:46 +0100

    Feature - add home file

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,10 @@
 <main>
     <section class="bg-green-300">
         <div class="container mx-auto py-40">
-            <h1 class="font-black text-white text-7xl">Tonka</h1>
-            <p class="text-white mt-14 text-xl">
-                👋 Hi, my name is Tonka, i'm a french developper.
-            </p>
-            <p class="text-white mt-5 text-xl">
-              I love making beautiful and unique websites with a nice UI.
-            </p>
+            <h1 class="font-black text-white text-7xl">{{ homeData.title }}</h1>
+              {{ homeData.description }}
             
-              <NuxtLink to="/contact" class=" transition inline-block mt-10 px-10 py-3 border border-white text-white text-xl hover:bg-white hover:text-green-300">Contact-me !</NuxtLink>
+              <NuxtLink to="/contact" class=" transition inline-block mt-10 px-10 py-3 border border-white text-white text-xl hover:bg-white hover:text-green-300">{{homeData.button}}</NuxtLink>
             
         </div>
     </section>
@@ -20,14 +15,8 @@
 
         </div>
         <div class="w-1/2">
-          <h2 class="font-black text-6xl  text-right">About me</h2>
-          <p class="mt-14 text-right">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque debitis, doloremque dignissimos ducimus voluptate odio quaerat tempore perspiciatis nostrum architecto commodi optio provident labore porro consequatur. Fugit distinctio velit explicabo ducimus voluptate accusantium adipisci pariatur inventore libero aperiam a aspernatur, ab sint ipsa sequi beatae vitae molestias enim cupiditate commodi, maiores minima. Autem veniam vel officiis? Praesentium sed, facilis incidunt tempore dolores libero deleniti vel quisquam officiis harum magnam suscipit possimus ipsum enim earum odio, dignissimos quibusdam natus accusantium molestiae sint expedita voluptatum. Debitis quos et, delectus dicta unde minus aliquid esse numquam aspernatur, beatae, nostrum voluptatum adipisci perspiciatis maxime explicabo est! Consectetur nam minima dicta nisi saepe nesciunt omnis provident animi labore, quam, eum magnam soluta amet aliquam voluptate eius suscipit at culpa odit, reiciendis doloribus praesentium voluptatem. Dignissimos, aut. Ab maiores, unde tenetur, numquam minima harum itaque animi temporibus fuga vero, voluptatibus consequuntur ex dolore eaque architecto necessitatibus!
-          </p>
-
-          <p class="mt-14 text-right">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque rerum, tempore laudantium exercitationem natus eaque aut reprehenderit. Necessitatibus, enim sapiente!
-          </p>
+          <h2 class="font-black text-6xl  text-right">{{ homeData.section-1-title }}</h2>
+          
 
         </div>
       </div>
@@ -54,7 +43,15 @@ export default {
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     };
+    
   },
+  async asyncData({$content, params}){
+      const homeData = await $content('home').fetch();
+
+      return {
+        homeData
+      }
+    }
 }
 </script>
 
